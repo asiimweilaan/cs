d94ec67a5e74e6e3418a0655398c037162cbda69
ilaan <benjailaan@gmail.com>
2022-01-10 20:33:39 +0300

    adding program.cs file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace CS
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Employee E = new Employee();
+            Console.WriteLine(E["Paul"]);
+        }
+    }
+    public class Employee{
+    public static List<Student> stud = new List<Student>(){
+        new Student(){Name="Paul",Age=18},
+        new Student(){Name="Roy",Age=21},
+        new Student(){Name="Bethany",Age=47}
+    };
+    public int this[string x]{
+        
+        set{stud.FirstOrDefault(stud=>stud.Name==x).Name= value.ToString();}
+        
+        get{return stud.FirstOrDefault(stud=>stud.Name==x).Age;}
+        
+
+    }
+
+    }
+    public class Student{
+    public string Name{get;set;}
+    public int Age{get;set;}
+
+    }
+}
